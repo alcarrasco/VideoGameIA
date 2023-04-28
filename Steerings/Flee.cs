7be8c9ff64ddb6b8fb0ce7f2291f111f0e67fb53
Alejandro Carrasco <carrasad@gmail.com>
2023-04-28 12:40:33 -0300

    Add files via upload

---
+++ Steerings/Flee.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : SteeringBehaviors
+{
+	protected override void Move()
+	{
+		if (destination != null)
+		{
+			direction = (destination.position - transform.position).normalized;
+
+			transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * rotSpeed);
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+	}
+}
---
+++ Steerings/SteeringBehaviors.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SteeringBehaviors : MonoBehaviour
+{
+
+	[SerializeField] protected float speed;
+	[SerializeField] protected float rotSpeed;
+	[SerializeField] protected Transform destination;
+	protected Vector3 direction;
+
+	bool isMoving;
+
+	public Transform Destination
+	{
+		get => destination;
+		set
+		{
+			destination = value;
+			if (destination != null) isMoving = true;
+			else isMoving = false;
+		}
+	}
+
+	public bool IsMoving => isMoving;
+
+	protected abstract void Move();
+
+	protected virtual void Update()
+	{
+		Move();
+	}
+
+}
