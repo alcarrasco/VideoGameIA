4894557594d371fbf60dbdd02b81672ebf80959b
Alejandro Carrasco <carrasad@gmail.com>
2023-05-05 20:20:54 -0300

    Add files via upload

---
+++ FSM/AttackState.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : State<NPC>
+{
+
+	BlackboardEntry<GameObject> target;
+	int LookOrIdle;
+
+	public AttackState(NPC owner, FSM<NPC> stateMachine) : base(owner, stateMachine)
+	{
+		target = owner.Mem.GetOrCreate<GameObject>("ObjectSeen");
+	}
+
+	public override void End()
+	{
+
+	}
+
+	public override void Execute()
+	{
+		if (owner.NPCHasDied)
+		{
+			fsm.SetState<DieState>();
+			return;
+		}
+		if(owner.RunningOutOfLife && target.value != null)
+		{
+			if (Vector3.Distance(owner.transform.position, target.value.transform.position) < 3f)
+			{
+				fsm.SetState<FleeState>();
+				return;
+			}
+		}
+		if (target.value == null)
+		{
+			//if(LookOrIdle == 0) fsm.SetState<LookForExitState>();
+			if (LookOrIdle == 0) fsm.SetState<EscapeState>();
+			else fsm.SetState<IdleState>();
+
+			return;
+		}
+
+		Vector3 dir = target.value.transform.position - owner.transform.position;
+		owner.WP.Shoot(owner.transform.position + owner.transform.forward, dir);
+
+	}
+
+	public override void Init()
+	{
+		LookOrIdle = Random.Range(0, 1);
+	}
+
+	public override void LastExecute()
+	{
+
+	}
+}
---
+++ FSM/DieState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieState : State<NPC>
+{
+	public DieState(NPC owner, FSM<NPC> stateMachine) : base(owner, stateMachine)
+	{
+
+	}
+
+	public override void End()
+	{
+
+	}
+
+	public override void Execute()
+	{
+
+	}
+
+	public override void Init()
+	{
+		Debug.Log("Adios mundo cruel!!!");
+	}
+
+	public override void LastExecute()
+	{
+
+	}
+}
